f09a6cfb7546ad94fd5e2fe6a21f0ba5ba5409f1
zidanegimiga <zidanegimiga@gmail.com>
2023-02-26 11:05:25 +0300

    config: set up api

--- server/index.js
+++ server/index.js
@@ -1,4 +1,12 @@
 const express = require('express')
 const app = express();
 
-const port = 4000
+const port = process.env.port || 4000 ;
+
+app.get('/', (req, res) =>{ 
+    res.send(`<h1>API is working</h1>`); 
+})
+
+app.listen(port, ()=>{
+    console.log(`Listening to server on port ${port}`)
+})
