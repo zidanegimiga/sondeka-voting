b139d3b99f058bb3a123afcf07f501609f818f93
zidanegimiga <zidanegimiga@gmail.com>
2023-02-26 16:10:06 +0300

    dep 1

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const router = express.Router();
 
-const port = process.env.PORT || 4000 ;
+const port = process.env.PORT || 3001 ;
 
 app.get('/', (req, res)=> {res.send("<head>Success</head><h1>Hello!</h1>")})
 
