f858f8c453f3a1f30fac0ef6ab951fe66dfa74d7
zidanegimiga <zidanegimiga@gmail.com>
2023-02-26 11:39:59 +0300

    config: test server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express();
+const router = express.Router();
 
 const port = process.env.port || 4000 ;
 
-app.get('/', (req, res) =>{ 
-    res.send(`<h1>API is working</h1>`); 
-})
+app.get('/', (req, res)=> {res.send("<head>Success</head><h1>Hello!</h1>")})
 
 app.listen(port, ()=>{
     console.log(`Listening to server on port ${port}`)
