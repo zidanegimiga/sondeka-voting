4b3aae011d9575110730fc473450c75438ee5931
zidanegimiga <zidanegimiga@gmail.com>
2023-02-26 16:25:39 +0300

    test

--- server/index.js
+++ server/index.js
@@ -6,6 +6,6 @@ const port = process.env.PORT || 3000 ;
 
 app.get('/', (req, res)=> {res.send("<head>Success</head><h1>Hello!</h1>")})
 
-app.listen(port, ()=>{
-    console.log(`Listening to server on port ${port}`)
+app.listen(process.env.PORT, ()=>{
+    console.log(`Success. Listening to server on port ${process.env.PORT}`)
 });
