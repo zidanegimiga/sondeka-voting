aec50415d7323a06e91bf07908cc834fdfd66598
zidanegimiga <zidanegimiga@gmail.com>
2023-02-26 16:24:44 +0300

    port

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express();
 const router = express.Router();
 
-const port = process.env.PORT || 3001 ;
+const port = process.env.PORT || 3000 ;
 
 app.get('/', (req, res)=> {res.send("<head>Success</head><h1>Hello!</h1>")})
 
 app.listen(port, ()=>{
     console.log(`Listening to server on port ${port}`)
-})
+});
