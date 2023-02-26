c785c1c8e6e8326a3e5696db36a9373e1bf6b59b
zidanegimiga <zidanegimiga@gmail.com>
2023-02-26 16:59:48 +0300

    test: can app send json

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+
+const dataRes = {
+    hello: 1,
+    world: 2
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json(dataRes)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
