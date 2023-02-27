5b1c51ad2aa1c0388df763e7c1604d0128ba74a9
Zidane Gimiga <zidanegimigatemp@gmail.com>
2023-02-27 21:55:17 +0300

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'https://www.youtube.com',
+    'https://vote.sondeka.org'
 ]
 
 module.exports = allowedOrigins
