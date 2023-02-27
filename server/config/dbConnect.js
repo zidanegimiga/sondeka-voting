daa03f3168cc1e1e4ad4569f5f77cf7fd07a0ca5
zidanegimiga <zidanegimigatemp@gmail.com>
2023-02-27 16:07:04 +0300

    config: connect to database

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
+const uri = "mongodb+srv://zidanegimiga:oVoZyAbjDM9Rv0aY@sondekavoting.xb1qayv.mongodb.net/?retryWrites=true&w=majority"
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI)
+        await mongoose.connect(uri);
     } catch (err) {
         console.log(err)
     }
--- server/index.js
+++ server/index.js
@@ -1,26 +1,30 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const path = require('path');
-const { logger } = require('./middleware/logger');
-const errorHandler = require('./middleware/errorHandler');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
-const PORT = process.env.PORT || 3500;
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+const { logger, logEvents } = require('./middlewares/logger')
+const errorHandler = require('./middlewares/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConnect')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3500
 
 
-app.use(logger);
+connectDB()
 
-app.use(cors(corsOptions));
+app.use(logger)
 
-app.use(express.json());
+app.use(cors(corsOptions))
 
-app.use(cookieParser());
+app.use(express.json())
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(cookieParser())
 
-app.use('/', require('./routes/root'));
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use('/', require('./routes/root'))
 
 app.all('*', (req, res) => {
     res.status(404)
@@ -35,4 +39,12 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
