f6ee32504906e310cb925d846de9234ff74be42d
zidanegimiga <zidanegimiga@gmail.com>
2023-02-28 23:58:37 +0300

    feat: create send email conf function

---
+++ server/utils/email/sendConfirmation.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+module.exports = async (options) => {
+    try{
+        const transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            service: process.env.EMAIL_SERVICE,
+            port: Number(process.env.EMAIL_PORT),
+            secure: Boolean(process.env.SECURE),
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        });
+
+        const {email, subject, html} = options
+
+        await transporter.sendMail(
+            {
+                from: process.env.EMAIL_USER,
+                to: email,
+                subject: subject,
+                html: html
+            }
+        )
+
+        console.log("Email sent successfully!")
+    } catch(error){}
+}
