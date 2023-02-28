0eaa2b5f366e02ac765a396e3c29d390bf74bb6c
zidanegimiga <zidanegimigatemp@gmail.com>
2023-02-28 15:46:58 +0300

    feat: create auth-mandatory routes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const usersController = require('../controllers/usersControllers')
+const verifyJWT = require('../middlewares/verifyJWT')
+
+router.use(verifyJWT)
+
+router.route('/')
+    .get(usersController.getAllUsers)
+    .patch(usersController.updateUser)
+    .delete(usersController.deleteUser)
+
+module.exports = router;
