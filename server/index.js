76db3ebcdb70fc51b591f74ef6bf020f0cb87090
zidanegimiga <zidanegimiga@gmail.com>
2023-02-28 23:53:17 +0300

    add email verification route

--- server/index.js
+++ server/index.js
@@ -32,6 +32,7 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
 app.post('/signup', usersController.createNewUser)
+app.post('/:id/verify/:token/', usersController.confirmEmail)
 app.use('/auth', require('./routes/authRoutes'))
 app.use('/users', require('./routes/userRoutes'))
 
