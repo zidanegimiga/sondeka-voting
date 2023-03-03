d94a6264615c062ad36a17e7913c03788344e025
Zidane Gimiga <zidanegimigatemp@gmail.com>
2023-03-03 14:15:43 +0300

    feat: create a rate limiter middleware function that limits requests based on IP address

--- server/routes/invalidLinkRoute.js
+++ server/routes/invalidLinkRoute.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const rateLimit = require('express-rate-limit');
 
-router.get('^/$|/invalidLink(.html)?', (req, res) => {
+// Reducing the number of reqs sent per unit time
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 50 // limit each IP to 50 requests per windowMs
+});
+
+router.get('^/$|/invalidLink(.html)?', limiter, (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'invalidLink.html'));
 });
 
