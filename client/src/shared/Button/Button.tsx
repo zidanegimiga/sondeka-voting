e1594580dea52e69d097bf9e8d2791d067c71b81
zidanegimiga <zidanegimigatemp@gmail.com>
2023-03-10 16:45:26 +0300

    feat: recreate Button Component

--- client/src/shared/Button/Button.tsx
+++ client/src/shared/Button/Button.tsx
@@ -1,11 +1,36 @@
-import styles from './Button.module.scss';
+import styles from "./Button.module.scss";
 
-const Button = ({text, action, color, type}) => {
-  return (
-    <button style={{"backgroundColor": color}} className={styles.button}>
-      {text}        
-    </button>
-  )
+interface ButtonProps {
+  text?: string;
+  active?: boolean;
+  variant?: string;
+  action?: any;
+  color: string;
+  children?: React.ReactNode;
+  icon?: boolean;
+  type?: any;
 }
+
+const Button = ({ text, action, color, type }: ButtonProps) => {
+  {
+    if (type == "submit") {
+      return (
+        <button style={{ backgroundColor: color }} className={styles.button} type={type}>
+          {text}
+        </button>
+      );
+    } else {
+      return (
+        <div
+          style={{ backgroundColor: color }}
+          className={styles.button}
+          onClick={action}
+        >
+          {text}
+        </div>
+      );
+    }
+  }
+};
 
 export default Button;
