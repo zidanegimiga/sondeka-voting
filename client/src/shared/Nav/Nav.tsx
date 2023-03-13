abe674f6cdff5471f8f48705d22c1b37bfc271e9
zidanegimiga <zidanegimigatemp@gmail.com>
2023-03-13 11:25:20 +0300

    feat: add logout button, use local storage

--- client/src/shared/Nav/Nav.tsx
+++ client/src/shared/Nav/Nav.tsx
@@ -3,37 +3,69 @@ import LogoWhite from "features/svgIcons/LogoWhite";
 import Menu from "features/svgIcons/menu";
 import styles from "./Nav.module.scss";
 import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 const Nav = () => {
   const [options, showOptions] = useState(false);
-  return (
-    <div className={styles.navContainer}>
-      <div className={styles.navWrapper}>
-        <Link href={"/"}>
-          <div>
-            <LogoWhite />
+  const [token, setToken] = useState<String>();
+  const router = useRouter()
+  function logout() {
+    window.localStorage.removeItem("token");
+    setToken(null)
+    router.push('/')
+    showOptions(false)    
+  }
+  useEffect(() => {
+    const getToken = window.localStorage.getItem("token");
+    setToken(getToken);
+  }, []);
+
+  if (typeof window !== undefined) {
+    return (
+      <div className={styles.navContainer}>
+        <div className={styles.navWrapper}>
+          <Link href={"/"}>
+            <div>
+              <LogoWhite />
+            </div>
+          </Link>
+          <div className={styles.title}>
+            50ND3KA <span> | AWARDS 2023</span>
           </div>
-        </Link>
-        <div className={styles.title}>
-          50ND3KA <span> | AWARDS 2023</span>
+          <div
+            className={styles.menuBtn}
+            onClick={() => {
+              showOptions(!options);
+            }}
+          >
+            <Menu />
+          </div>
         </div>
-        <div className={styles.menuBtn} onClick={()=>{showOptions(!options)}}>
-          <Menu />
-        </div>
+        {options && (
+          <div className={styles.navOptions}>
+            {!token && (
+              <>
+                <div className={styles.navLink}>
+                  <Link href={"/signup"}>Sign Up</Link>
+                </div>
+                <div className={styles.navLink}>
+                  <Link href={"/login"}>Log In</Link>
+                </div>
+              </>
+            )}
+            {token && (
+              <div className={styles.navLink} onClick={() => logout()}>
+                <div>Log Out</div>
+              </div>
+            )}
+          </div>
+        )}
       </div>
-      {options && (
-        <div className={styles.navOptions}>
-          <div className={styles.navLink}>
-            <Link href={"/signup"}>Sign Up</Link>
-          </div>
-          <div className={styles.navLink}>
-            <Link href={"/login"}>Log In</Link>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+    );
+  } else {
+    return <div>Window not defined</div>;
+  }
 };
 
 export default Nav;
