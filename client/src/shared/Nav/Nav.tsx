63110ec62553361dc0b7a2c5d3455c4e21589d30
zidanegimiga <zidanegimigatemp@gmail.com>
2023-03-13 10:17:31 +0300

    feat: create navbar menu

--- client/src/shared/Nav/Nav.tsx
+++ client/src/shared/Nav/Nav.tsx
@@ -2,15 +2,38 @@ import Logo from "features/svgIcons/logoBlack";
 import LogoWhite from "features/svgIcons/LogoWhite";
 import Menu from "features/svgIcons/menu";
 import styles from "./Nav.module.scss";
+import Link from "next/link";
+import { useState } from "react";
 
 const Nav = () => {
-  return(
-    <div className={styles.navWrapper}>
-      <div><LogoWhite /></div>
-      <div className={styles.title}>50ND3KA <span> | AWARDS 2023</span></div>
-      <div className={styles.menuBtn}><Menu/></div>
+  const [options, showOptions] = useState(false);
+  return (
+    <div className={styles.navContainer}>
+      <div className={styles.navWrapper}>
+        <Link href={"/"}>
+          <div>
+            <LogoWhite />
+          </div>
+        </Link>
+        <div className={styles.title}>
+          50ND3KA <span> | AWARDS 2023</span>
+        </div>
+        <div className={styles.menuBtn} onClick={()=>{showOptions(!options)}}>
+          <Menu />
+        </div>
+      </div>
+      {options && (
+        <div className={styles.navOptions}>
+          <div className={styles.navLink}>
+            <Link href={"/signup"}>Sign Up</Link>
+          </div>
+          <div className={styles.navLink}>
+            <Link href={"/login"}>Log In</Link>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 };
 
 export default Nav;
