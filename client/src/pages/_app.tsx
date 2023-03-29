978d4a9a2e1704345c7f98dacc9852060c52fcab
zidanegimiga <zidanegimigatemp@gmail.com>
2023-03-29 11:12:16 +0300

    feat: wrap app with AuthProvider

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import { FC } from 'react';
 import { Provider } from 'react-redux';
+import AuthProvider from 'admin-auth-context';
 import { AppProps } from 'next/app';
 import NextNProgress from 'nextjs-progressbar';
 import '../styles/app.scss';
 
 const App:FC = ({ Component, pageProps }:AppProps) => {
   return (
-    <>
+    //@ts-ignore
+    <AuthProvider>
       <NextNProgress color="#29D" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true}/>
       <Component {...pageProps} />;
-    </>
+    </AuthProvider>
   );
 }
 
