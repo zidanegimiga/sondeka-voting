4fb239ffc4bec08200749c5ed9fb3632ba54ec33
zidanegimiga <zidanegimigatemp@gmail.com>
2023-03-29 11:11:39 +0300

    feat: create admin auth context

---
+++ client/src/admin-auth-context.tsx
@@ -0,0 +1,40 @@
+import { createContext, FC, useState } from "react";
+
+interface AuthContextType {
+  token: string;
+  login: (newToken: string) => void;
+  logout: () => void;
+  isAuthenticated: boolean;
+}
+
+export const AuthContext = createContext<AuthContextType>({
+  token: "",
+  login: () => {},
+  logout: () => {},
+  isAuthenticated: false,
+});
+
+// @ts-ignore
+const AuthProvider: FC = ({ children }) => {
+  const [token, setToken] = useState("");
+
+  const login = (newToken: string) => {
+    setToken(newToken);
+    localStorage.setItem("token", newToken);
+  };
+
+  const logout = () => {
+    setToken("");
+    localStorage.removeItem("token");
+  };
+
+  const isAuthenticated = !!token;
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
