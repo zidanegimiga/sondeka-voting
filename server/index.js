6171d93cb8abc8182f957682796676fb3c36120d
zidanegimiga <zidanegimigatemp@gmail.com>
2023-04-05 16:43:11 +0300

    reafctor: initialise session middleware in required routes only

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config({ path: __dirname + '/.env' })
 
 const express = require('express')
-const session = require('express-session')
 const app = express()
 const path = require('path')
 const { logger, logEvents } = require('./middlewares/logger')
@@ -38,38 +37,6 @@ app.post('/signup', usersController.createNewUser)
 app.get('/:id/verify/:token/', usersController.confirmEmail)
 app.post('/reverify', usersController.resendVerificationLink)
 
-// Session settings for admin
-const sessionStore = MongoStore.create({
-    mongoUrl: `${process.env.DATABASE_URI}?retryWrites=true&w=majority`,
-    secret: process.env.ADMIN_SECRET_KEY,
-    ttl: 14 * 24 * 60 * 60,
-    autoRemove: 'native'
-});
-
-sessionStore.on("error", function (err) {
-    console.log("Session Error: ", err);
-});
-
-const sessionSettings = {
-    store: sessionStore,
-    name: "adminAuth",
-    secret: process.env.STORE_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      maxAge: 3600000, // 1 hour in milliseconds
-      httpOnly: true,
-      // secure: true,
-    },
-}
-
-app.use(session(sessionSettings));
-
-// A silly route i will stop using :)
-app.get('/test', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'test.html'))
-})
-
 //Categories Routes
 app.use('/categories', require('./routes/categories/categoryRoutes'))
 
@@ -77,9 +44,7 @@ app.use('/categories', require('./routes/categories/categoryRoutes'))
 app.use('/nominees', require('./routes/nominees/nomineesRoutes'))
 
 // Admin Routes
-app.use('/admin/authentication', require('./routes/admin/adminAuthRoutes'))
-app.use('/admin/categories', require('./routes/admin/adminCategoriesRoutes'))
-app.use('/admin/nominees', require('./routes/admin/adminNomineesRoutes'))
+app.use('/admin', require('./routes/admin/adminRoutes'))
 
 app.all('*', (req, res) => {
     res.status(404)
