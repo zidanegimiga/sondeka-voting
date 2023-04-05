560523e1a84ce4336a97140d0b08a2935ea4e4cf
zidanegimiga <zidanegimigatemp@gmail.com>
2023-04-05 16:41:55 +0300

    refactor: consolidate all admin routes in a single file

--- server/routes/admin/adminAuthRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminAuthController = require('../../controllers/admin/adminAuthController');
-
-router.route('/login').post(adminAuthController.login);
-router.route('/logout').get(adminAuthController.logout);
-
-module.exports = router
