606b1d7cb4ea0bd9c65af50848643bb24f9d3084
zidanegimiga <zidanegimigatemp@gmail.com>
2023-04-13 11:50:15 +0300

    reinstate AuthProvider

--- client/src/admin-auth-context.tsx
+++ client/src/admin-auth-context.tsx
@@ -1,40 +1,40 @@
-// import { createContext, FC, useState } from "react";
+import { createContext, FC, useState } from "react";
 
-// interface AuthContextType {
-//   token: string;
-//   login: (newToken: string) => void;
-//   logout: () => void;
-//   isAuthenticated: boolean;
-// }
+interface AuthContextType {
+  token: string;
+  login: (newToken: string) => void;
+  logout: () => void;
+  isAuthenticated: boolean;
+}
 
-// export const AuthContext = createContext<AuthContextType>({
-//   token: "",
-//   login: () => {},
-//   logout: () => {},
-//   isAuthenticated: false,
-// });
+export const AuthContext = createContext<AuthContextType>({
+  token: "",
+  login: () => {},
+  logout: () => {},
+  isAuthenticated: false,
+});
 
-// // @ts-ignore
-// const AuthProvider: FC = ({ children }) => {
-//   const [token, setToken] = useState("");
+// @ts-ignore
+const AuthProvider: FC = ({ children }) => {
+  const [token, setToken] = useState("");
 
-//   const login = (newToken: string) => {
-//     setToken(newToken);
-//     localStorage.setItem("admin-token", newToken);
-//   };
+  const login = (newToken: string) => {
+    setToken(newToken);
+    localStorage.setItem("admin-token", newToken);
+  };
 
-//   const logout = () => {
-//     setToken("");
-//     localStorage.removeItem("admin-token");
-//   };
+  const logout = () => {
+    setToken("");
+    localStorage.removeItem("admin-token");
+  };
 
-//   const isAuthenticated = !!token;
+  const isAuthenticated = !!token;
 
-//   return (
-//     <AuthContext.Provider value={{ token, login, logout, isAuthenticated }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
+  return (
+    <AuthContext.Provider value={{ token, login, logout, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
-// export default AuthProvider;
+export default AuthProvider;
