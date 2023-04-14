f6ea0f7ca3ee0cc326243676c11f3e9fdbf9b121
zidanegimiga <zidanegimigatemp@gmail.com>
2023-04-14 15:40:08 +0300

    feat: fetch and display nominee data

--- client/src/pages/admin/nominees.tsx
+++ client/src/pages/admin/nominees.tsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "shared/Nav/Nav";
 import styles from "../../styles/adminVotersPanel.module.scss";
-import Image from 'next/image'
+import Image from "next/image";
 import AdminContentWrapper from "shared/AdminContentWrapper/AdminContentWrapper";
+import { useRouter } from "next/router";
 
 const Dashboard = () => {
+  const [nomineesData, setNomineesData] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const accessToken = window.localStorage.getItem("admin-token");
+    const categoryId = router.query.categoryId;
+
+    const fetchNomineesData = async () => {
+      const res = await fetch(
+        `http://localhost:3500/admin/nominees/allNominees`,
+        {
+          headers: {
+            authorization: accessToken,
+          },
+        }
+      );
+
+      const data = await res.json();
+      console.log("Nominee Data: ", data);
+
+      if (data && data.message === "Forbiden!") {
+        setNomineesData([]);
+      } else {
+        setNomineesData(data);
+      }
+    };
+
+    accessToken ? fetchNomineesData() : router.push("/admin/login-admin");
+  }, []);
+
   return (
     <div className={styles.pageWrapper}>
       <Nav />
@@ -12,14 +43,17 @@ const Dashboard = () => {
         <div className={styles.contentWrapper}>
           <AdminContentWrapper>
             <div className={styles.header}>
-              <h1>Categories</h1>
+              <h1>Nominees</h1>
               <div className={styles.count}>
                 <p>Voters Count:</p>
-                <span>115k</span>
+                <span>{nomineesData.length + 1}</span>
               </div>
             </div>
             <div className={styles.warning}>
-                <p>For Security purposes, open the admin panel on your desktop instead</p>
+              <p>
+                For Security purposes, open the admin panel on your desktop
+                instead
+              </p>
             </div>
             <div className={styles.tableWrapper}>
               <table className={styles.styledTable}>
@@ -32,20 +66,40 @@ const Dashboard = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>1</td>
-                    <td>
-                        <div className={styles.nomineePictureWrapper} style={{display: "flex"}}>
-                            <Image src={'/nominee.png'} width={60} height={72} alt="nominee"/>
-                            <div className={styles.nomineeDetails} style={{marginLeft: "16px"}}>
-                                <p className={styles.nomineeName}  style={{fontWeight: "700", color: "white"}}>Kaskazini</p>
-                                <p className={styles.nomineeEmail}>Kaskazini@example.com</p>
-                            </div>
+                  {nomineesData.map((nominee, index) => (
+                    <tr key={index}>
+                      <td>{index + 1}</td>
+                      <td>
+                        <div
+                          className={styles.nomineePictureWrapper}
+                          style={{ display: "flex" }}
+                        >
+                          <Image
+                            src={"/nominee.png"}
+                            width={60}
+                            height={72}
+                            alt="nominee"
+                          />
+                          <div
+                            className={styles.nomineeDetails}
+                            style={{ marginLeft: "16px" }}
+                          >
+                            <p
+                              className={styles.nomineeName}
+                              style={{ fontWeight: "700", color: "white" }}
+                            >
+                              {nominee.name}
+                            </p>
+                            <p className={styles.nomineeEmail}>
+                              {nominee.description.slice(0, 20)}...
+                            </p>
+                          </div>
                         </div>
-                    </td>
-                    <td>Poetry</td>
-                    <td>95</td>
-                  </tr>
+                      </td>
+                      <td>{nominee.categoryName}</td>
+                      <td>{nominee.votes}</td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
