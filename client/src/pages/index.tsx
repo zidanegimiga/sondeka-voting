7e169fee8a3dbcfd0bab6cea6f7d127ab62d72a0
zidanegimiga <zidanegimigatemp@gmail.com>
2023-04-20 17:44:02 +0300

    feat: scroll component

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Nav from "shared/Nav";
 import Hero from "features/Home/Hero";
 import Categories from "features/Home/Categories";
+import DownArrow from "features/svgIcons/downArrow";
 
 export default function Index({categories}) {
   return (
@@ -13,6 +14,13 @@ export default function Index({categories}) {
       <div className={styles.pageWrapper}>
         <Nav/>
         <Hero />
+        <div className={styles.scroller}>
+          <div className={styles.iconWrapper}>
+            <DownArrow/>
+          </div>
+          <div className={styles.scrollText}>SCROLL</div>
+          <div className={styles.divider}></div>
+        </div>
         {/* <Categories /> */}
 
       </div>
