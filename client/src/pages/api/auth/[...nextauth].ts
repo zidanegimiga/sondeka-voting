d0a7ac95a45d8059329fa9e6fb3d2c0984edcdf8
zidanegimiga <zidanegimigatemp@gmail.com>
2023-04-24 10:27:17 +0300

    config: SET NEXTAUTH SECRET KEY

--- client/src/pages/api/auth/[...nextauth].ts
+++ client/src/pages/api/auth/[...nextauth].ts
@@ -11,7 +11,7 @@ export const authOptions = {
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.JWT_SECRET,
   pages:{
     signIn: '/oAuthLogin'
   }
