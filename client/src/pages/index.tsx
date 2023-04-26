5ebd13bed2ce42fb38e27245e9d627ce7886960a
zidanegimiga <zidanegimigatemp@gmail.com>
2023-04-26 16:10:49 +0300

    Add social media icons

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -5,53 +5,69 @@ import Hero from "features/Home/Hero";
 import Categories from "features/Home/Categories";
 import DownArrow from "features/svgIcons/downArrow";
 import Countdown from "features/Home/Countdown/Countdown";
-import Link from 'next/link'
-import SocialsButton from '../features/Home/SocialsButton'
+import Link from "next/link";
+import SocialsButton from "../features/Home/SocialsButton";
 
-export default function Index({data}) {
+export default function Index({ data }) {
   return (
     <div className={styles.homeWrapper}>
       <Head>
         <title>Voting - sondeka </title>
       </Head>
       <div className={styles.pageWrapper}>
-        <Nav/>
+        <Nav />
         <Hero />
         <div className={styles.scroller}>
           <div className={styles.iconWrapper}>
-            <DownArrow/>
+            <DownArrow />
           </div>
           <div className={styles.scrollText}>SCROLL</div>
           <div className={styles.divider}></div>
         </div>
         <div className={styles.countdownContainer}>
           <div className={styles.countdownContainerTitle}>VOTING BEGINS IN</div>
-          <Countdown/>
+          <Countdown />
         </div>
         <div className={styles.subLogo}>
-          <div className={styles.sondekaTitle}> 5OND3KA <span>| AWARDS 2023</span></div>
+          <div className={styles.sondekaTitle}>
+            {" "}
+            5OND3KA <span>| AWARDS 2023</span>
+          </div>
         </div>
         <div className={styles.footerLayer1}>
-          <Link href={'/privacy'}> <p>Privacy Policy</p> </Link>
-          <Link href={'https://creativesgarage.org'}> <p> Creatives Garage </p> </Link>
-          <p><a href = "mailto: awesome@creativesgarage.org">awesome@creativesgarage.org</a></p>
+          <Link href={"/privacy"}>
+            {" "}
+            <p>Privacy Policy</p>{" "}
+          </Link>
+          <Link href={"https://creativesgarage.org"}>
+            {" "}
+            <p> Creatives Garage </p>{" "}
+          </Link>
+          <p>
+            <a href="mailto: awesome@creativesgarage.org">
+              awesome@creativesgarage.org
+            </a>
+          </p>
         </div>
-        {/* <div className={styles.socials}>
-          <SocialsButton />          
-        </div> */}
+        <div className={styles.socials}>
+          <div className={styles.socialsTitle}>SOCIALS</div>
+          <SocialsButton />
+        </div>
         {/* <Categories /> */}
       </div>
     </div>
-    );
+  );
 }
 
 export const getServerSideProps = async () => {
-  const res = await fetch("https://sondeka-voting-api.cyclic.app/categories/allCategories")
-  const data = await res.json()
+  const res = await fetch(
+    "https://sondeka-voting-api.cyclic.app/categories/allCategories"
+  );
+  const data = await res.json();
 
   return {
     props: {
       data,
     },
-  }
-}
+  };
+};
