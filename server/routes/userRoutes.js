b78272d7bbdc7f68b2bbfced0c5c194294f9ea40
zidanegimiga <zidanegimigatemp@gmail.com>
2023-05-08 12:06:45 +0300

    create userId route and create Account route before

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router()
 const usersController = require('../controllers/users/usersControllers')
 const verifyJWT = require('../middlewares/verifyJWT')
 
-router.use(verifyJWT)
+// router.use(verifyJWT)
 
 router.route('/')
     .get(usersController.getAllUsers)
     .patch(usersController.updateUser)
     .delete(usersController.deleteUser)
+
+router.route('/:name').get(usersController.getUserId)
 
 module.exports = router;
