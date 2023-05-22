2b733f132e7fc801e4261831e47c905e27b882a3
zidanegimiga <zidanegimigatemp@gmail.com>
2023-05-22 13:01:27 +0300

    voting

--- server/routes/voteRoute.js
+++ server/routes/voteRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const verifyJWT = require('../middlewares/verifyJWT')
 const voteController = require('../controllers/users/voteController');
 
-router.use(verifyJWT)
+// router.use(verifyJWT)
 
 router.route('/').post(voteController.vote)
 
