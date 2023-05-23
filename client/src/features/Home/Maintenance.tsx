f9105fd2eccfbf375ac9a9b545570eaa70c11c6e
zidanegimiga <zidanegimigatemp@gmail.com>
2023-05-23 09:25:25 +0300

    maintenence mode

---
+++ client/src/features/Home/Maintenance.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Maintenance = ({children}) => {
+  return (
+    <div style={{backgroundColor: "#FFFFFF", width: "100vw", height: "100vh"}}>
+    </div>
+  )
+}
+
+export default Maintenance
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -8,25 +8,32 @@ import { Analytics } from "@vercel/analytics/react";
 import { SessionProvider, useSession } from "next-auth/react";
 import Cursor from "shared/Cursor";
 import { VoterContext, VoterProvider } from "global/VoterContext";
+import Maintenance from "features/Home/Maintenance";
 
 const App: FC = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
-  return (
-    <VoterProvider>
-      <NextNProgress
-        color="#29D"
-        startPosition={0.3}
-        stopDelayMs={200}
-        height={3}
-        showOnShallow={true}
-      />
-      <SessionProvider session={session}>
-        <AuthProvider>
-          <Component {...pageProps} />
-          <Analytics />
-        </AuthProvider>
-      </SessionProvider>
-    </VoterProvider>
-  );
+  if(process.env.MAINTENANCE === "yes"){
+    return(
+      <Maintenance></Maintenance>
+    )
+  } else if(process.env.MAINTENANCE === "no"){
+    return (
+      <VoterProvider>
+        <NextNProgress
+          color="#29D"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+        />
+        <SessionProvider session={session}>
+          <AuthProvider>
+            <Component {...pageProps} />
+            <Analytics />
+          </AuthProvider>
+        </SessionProvider>
+      </VoterProvider>
+    );
+  }
 };
 
 export default App;
