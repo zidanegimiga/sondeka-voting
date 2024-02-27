a83331d23ff10359ea86dfe2a334f7b05be9031c
Zidane Gimiga <zidanegimigatemp@gmail.com>
2024-02-27 12:52:24 +0300

    Update loginLimiter.js

--- server/middlewares/loginLimiter.js
+++ server/middlewares/loginLimiter.js
@@ -4,8 +4,7 @@ const { logEvents } = require('./logger')
 const loginLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
     max: 5, // Limit each IP to 5 login requests per `window` per minute
-    message:
-        { message: 'Too many login attempts from this IP, please try again after a 60 second pause' },
+    message: { message: 'Too many login attempts from this IP, please try again after a 60 second pause' },
     handler: (req, res, next, options) => {
         logEvents(`Too Many Requests: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
         res.status(options.statusCode).send(options.message)
